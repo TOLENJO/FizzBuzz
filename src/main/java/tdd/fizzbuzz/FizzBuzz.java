2c58cf8491cf602119d38e4ae33025f9c3e5439d
TOLENJO <tolenjo@oocl.com>
2022-09-01 14:38:22 +0800

    refactor: Introduced String Constants

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,12 +1,16 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String WHIZZ = "Whizz";
+
     public String countOf(int order) {
         String orderMessage = "";
 
-        if (order % 3 == 0) orderMessage += "Fizz";
-        if (order % 5 == 0) orderMessage += "Buzz";
-        if (order % 7 == 0) orderMessage += "Whizz";
+        if (order % 3 == 0) orderMessage += FIZZ;
+        if (order % 5 == 0) orderMessage += BUZZ;
+        if (order % 7 == 0) orderMessage += WHIZZ;
         if (orderMessage.isEmpty()) orderMessage = String.valueOf(order);
 
         return orderMessage;
