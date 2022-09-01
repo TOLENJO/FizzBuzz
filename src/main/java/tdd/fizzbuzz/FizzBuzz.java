d4fbb95299c60e34eb3abd8c3b549bc4c98b535a
TOLENJO <tolenjo@oocl.com>
2022-09-01 14:01:26 +0800

    test: Added Test for Normal Case

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,7 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public String countOf(int order) {
+        return null;
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,20 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_order_number_when_countOf_case_given_normal_case() {
+        //Given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String expected = "1";
+
+        //When
+        String actual = fizzBuzz.countOf(0);
+
+        //Then
+        assertEquals(expected, actual);
+    }
 }
