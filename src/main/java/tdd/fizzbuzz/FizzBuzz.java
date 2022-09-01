3a6672ad338b44ab801aaa96194502bdbb715ccf
TOLENJO <tolenjo@oocl.com>
2022-09-01 14:28:45 +0800

    feat: Implemented Code for Special Numbers Combination

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,17 +2,21 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOf(int order) {
-        String orderMessage;
+        String orderMessage = "";
 
         if (order % 3 == 0 ) {
             orderMessage = "Fizz";
-        } else if (order % 5 == 0) {
-            orderMessage = "Buzz";
-        } else if (order % 7 == 0) {
-            orderMessage = "Whizz";
-        } else {
+        }
+        if (order % 5 == 0) {
+            orderMessage += "Buzz";
+        }
+        if (order % 7 == 0) {
+            orderMessage += "Whizz";
+        }
+        if (orderMessage.isEmpty()) {
             orderMessage = String.valueOf(order);
         }
+
         return orderMessage;
     }
 }
