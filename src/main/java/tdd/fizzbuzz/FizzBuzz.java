e4d0076b7405211adf930184f3fe9e7a2b717bdb
TOLENJO <tolenjo@oocl.com>
2022-09-01 14:09:16 +0800

    feat: Implemented Code for Fizz Case

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,13 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOf(int order) {
-        return String.valueOf(order);
+        String orderMessage;
+
+        if (order % 3 == 0 ) {
+            orderMessage = "Fizz";
+        } else {
+            orderMessage = String.valueOf(order);
+        }
+        return orderMessage;
     }
 }
